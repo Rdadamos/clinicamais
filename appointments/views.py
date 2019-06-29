b683860dbfa394201ba114620ecae81fb6e18199
Rdadamos <rodrigodadamos@gmail.com>
2019-06-29 19:40:18 -0300

    Initial login page

--- appointments/views.py
+++ appointments/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Attendant
 
-# Create your views here.
+def index(request):
+    latest_question_list = Attendant.objects.order_by('name')
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'appointments/index.html', context)
